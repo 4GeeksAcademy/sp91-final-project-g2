21d975d95f50286957c7a72122110d08a548bf4f
Luis Alexander Borjas Zamora <119633920+alex22bo@users.noreply.github.com>
2025-02-17 20:44:24 +0000

    feat: tercera versión de users

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Comments
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
@@ -154,14 +154,83 @@ def user_account(id):
         return response_body, 200
     
 
-# Endpoint para administrador
+# Endpoints para el rol de administrador
+# Permite al Administrador obtener los datos de los vendedores y clientes y a la vez editarlos y/o darlos de baja.
 @api.route('/admin/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 @jwt_required()
-def admin_account(id):
+def admin_user_management(id):
     response_body = {}
     additional_claims = get_jwt()
     if not additional_claims.get('is_admin', False):
         response_body['message'] = 'Acceso Denegado'
         return response_body, 403
-    rows = db.session.execute(db.select(Users)).scalars()
-    return response_body, 200    
+    row = db.session.execute(db.select(Users).where(Users.id == id)).scalars()
+    if not row:
+        response_body['message'] = 'Usuario no encontrado'
+        return response_body, 404
+    if request.method == 'GET':
+        response_body['results'] = row.serialize()
+        return response_body, 200
+    if response_body == 'PUT':
+        data = request.json
+        row.first_name = data.get('first_name', row.first_name)
+        row.last_name = data.get('last_name', row.last_name)
+        row.phone = data.get('phone', row.phone)
+        row.address = data.get('address', row.address)
+        row.is_customer = data.get('is_customer', row.is_customer)
+        row.is_vendor = data.get('is_vendor', row.is_vendor)
+        row.is_active = data.get('is_active', row.is_active)
+        db.session.commit()
+        response_body['message'] = 'Usuario actualizado existosamente'
+        response_body['results'] = row.serialize()
+        return response_body, 200
+    if request.method == 'DELETE':
+        db.session.delete(row)
+        db.session.commit()
+        response_body['message'] = 'Usuario eliminado'
+        return response_body, 200
+
+
+# Permite al Administrador obtener los comentarios de un usuario especifico.
+@api.route('/admin/user-comments/<int:user_id>', methods=['GET'])
+@jwt_required()
+def admin_get_comments_management(user_id):
+    response_body = {}
+    additional_claims = get_jwt()
+    if not additional_claims.get('is_admin', False):
+        response_body['message'] = 'Acceso Denegado'
+        return response_body, 403
+    rows = db.session.execute(db.select(Comments).where(Comments.user_id == user_id)).scalars()
+    comments_list = [row.serialize() for row in rows]
+    response_body['message'] = f'Comentarios del usuario {user_id}'
+    request['results'] = comments_list
+    return response_body, 200
+
+# Permite al Administrador editar o eliminar un comentario de un usuario especifico.
+@api.route('/admin/user-comments/<int:user_id>/<int:comment_id', methods=['PUT', 'DELETE'])
+@jwt_required()
+def admin_user_comments_management(user_id, comment_id):
+    response_body = {}
+    aditional_claims = get_jwt()
+    if not aditional_claims.get('is_admin', False):
+        response_body['message'] = 'Acceso Denegado'
+        return response_body, 403
+    comment = db.session.execute(db.select(Comments).where(Comments.id == comment_id, Comments.user_id == user_id)).scalar()
+    if not comment:
+        response_body['message'] = 'Comentario no encontrado'
+        return response_body, 404
+    if request.method == 'PUT':
+        data = request.json
+        comment.title = data.get('title', comment.title)
+        comment.description = data.get('description', comment.description)
+        # Confirmar si va a llevar Media para incluirlo
+        db.session.commit()
+        response_body['message'] = 'Comentario editado'
+        response_body['results'] = comment.serialize()
+        response_body, response_body, 200
+    if request.method == 'DELETE':
+        db.session.delente(comment)
+        db.session.commit()
+        response_body['message'] = 'Comentario eliminado'
+        return response_body, 200
+
