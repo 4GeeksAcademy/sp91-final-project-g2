3f54912023cdaf8b2491213702ce171a74c5a3c5
Cristian Chacon <cristianchacohn@gmail.com>
2025-03-08 09:03:18 +0000

    algunos avances

---
+++ src/front/js/pages/Products/ProductCard.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../../store/appContext";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+export const ProductCard = ({ product }) => {
+  const { actions } = useContext(Context);
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className="col-lg-3 col-md-4 col-sm-6 mb-4 mt-2"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="card product-card text-center">
+        <div className="card-body">
+          <h5 className="card-title">{product.name}</h5>
+          <p className="card-text">{product.description}</p>
+          <h6 className="price">${product.price.toFixed(2)}</h6>
+          <button
+            className={`btn btn-add-cart ${isHovered ? "hovered" : ""}`}
+            onClick={() => actions.addToCart(product)}
+          >
+            <i className="fa fa-cart-plus"></i> Añadir al carrito
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Validación de props
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    price: PropTypes.number.isRequired,
+    photo: PropTypes.string,
+  }).isRequired,
+};
+
+export default ProductCard;
